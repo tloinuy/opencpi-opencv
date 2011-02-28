3ac77cd88f6d96304a528fd0bbe8bae8150097f1
Tony J. Liu <tonyliu@localhost.localdomain>
2011-02-28 03:59:58 -0500

    blur/median RCC workers

---
+++ opencpi/components/median.rcc/median.c
@@ -0,0 +1,125 @@
+/*
+ * THIS FILE WAS ORIGINALLY GENERATED ON Mon Feb 28 03:51:23 2011 EST
+ * BASED ON THE FILE: median.xml
+ * YOU ARE EXPECTED TO EDIT IT
+ *
+ * This file contains the RCC implementation skeleton for worker: median
+ */
+#include <string.h>
+#include "median_Worker.h"
+
+// Algorithm specifics:
+typedef uint8_t Pixel;      // the data type of pixels
+#define MAX UINT8_MAX       // the maximum pixel value
+#define KERNEL_SIZE 3       // the size of the kernel
+
+// Define state between runs.  Will be initialized to zero for us.
+#define LINE_BYTES (p->width * sizeof(Pixel))
+#define HISTORY_SIZE KERNEL_SIZE
+typedef struct {
+  unsigned inLine;                 // The next line# of input I expect
+  RCCBuffer buffers[HISTORY_SIZE]; // buffer history (ring)
+} MedianState;
+
+static uint32_t sizes[] = {sizeof(MedianState), 0};
+
+MEDIAN_METHOD_DECLARATIONS;
+RCCDispatch median = {
+  /* insert any custom initializations here */
+	.memSizes = sizes,
+  MEDIAN_DISPATCH
+};
+
+// Compute one line of output
+inline static void
+doLine(Pixel *l0, Pixel *l1, Pixel *l2, Pixel *out, unsigned width) {
+  unsigned i; // don't depend on c99 yet
+  for (i = 1; i < width - 1; i++) {
+		Pixel p[] = {
+			l0[i-1], l0[i], l0[i+1],
+			l1[i-1], l1[i], l1[i+1],
+			l2[i-1], l2[i], l2[i+1]
+		};
+		// sort increasing
+		unsigned j, k;
+		for(j = 0; j < 9; j++) {
+			for(k = j + 1; k < 9; k++) {
+				if(p[k] < p[j]) {
+					Pixel t = p[k];
+					p[k] = p[j];
+					p[j] = t;
+				}
+			}
+		}
+		// median of 3x3 region
+    out[i] = p[4];
+  }
+  out[0] = out[width-1] = 0; // boundary conditions
+}
+
+// A run condition for flushing zero-length message
+// Basically we only wait for an output buffer, not an input
+// This will be unnecessary when we have a separate EOS indication.
+static RCCPortMask endMask[] = {1 << MEDIAN_OUT, 0};
+static RCCRunCondition end = {endMask, 0, 0};
+
+/*
+ * Methods to implement for worker median, based on metadata.
+*/
+
+static RCCResult run(RCCWorker *self,
+                     RCCBoolean timedOut,
+                     RCCBoolean *newRunCondition) {
+  MedianProperties *p = self->properties;
+  MedianState *s = self->memories[0];
+  RCCPort *in = &self->ports[MEDIAN_IN],
+          *out = &self->ports[MEDIAN_OUT];
+  const RCCContainer *c = self->container;
+  
+  (void)timedOut;
+
+  // End state:  just send the zero length message to indicate "done"
+  // This will be unnecessary when EOS indication is fixed
+  if (self->runCondition == &end) {
+    out->output.length = 0;
+    c->advance(out, 0);
+    return RCC_DONE;
+  }
+
+	// Current buffer
+	unsigned cur = s->inLine % HISTORY_SIZE;
+
+	// First line: do nothing
+	// Second line
+	if(s->inLine == 1) {
+		memset(out->current.data, 0, LINE_BYTES);
+    out->output.length = LINE_BYTES;
+		c->advance(out, LINE_BYTES);
+	}
+	// Middle line
+	else if(s->inLine > 1) {
+		doLine(s->buffers[(cur - 2 + HISTORY_SIZE) % HISTORY_SIZE].data,
+			s->buffers[(cur - 1 + HISTORY_SIZE) % HISTORY_SIZE].data,
+			in->current.data,
+			out->current.data,
+			p->width);
+    out->output.length = LINE_BYTES;
+		c->advance(out, LINE_BYTES);
+	}
+
+	// Go to next
+	unsigned prev = (cur - 2 + HISTORY_SIZE) % HISTORY_SIZE;
+	if(s->inLine < HISTORY_SIZE - 1)
+		c->take(in, NULL, &s->buffers[cur]);
+	else
+		c->take(in, &s->buffers[prev], &s->buffers[cur]);
+	s->inLine++;
+
+  // Arrange to send the zero-length message after the last line of last image
+  // This will be unnecessary when EOS indication is fixed
+  if (in->input.length == 0) {
+    self->runCondition = &end;
+    *newRunCondition = 1;
+  }
+  return RCC_OK;
+}
